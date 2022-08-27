1db720a28253e34df3ca1a11ec15c8e95cccb5d8
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-08-27 22:45:26 +0530

    dynamic akki

---
+++ form/brain.js
@@ -0,0 +1,9 @@
+function Closer() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("darker").style.display = "none";
+}
+
+function Opener(){
+    document.getElementById("popup").style.display = "flex" ;
+    document.getElementById("darker").style.display = "block";
+}
