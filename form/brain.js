48ae14112dd405fe4ee38bae690ecdcc8f1a26b2
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-09-07 03:04:50 +0530

    Modified

--- form/brain.js
+++ form/brain.js
@@ -11,7 +11,7 @@ var x = setInterval(function()
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  document.getElementById("timer").innerHTML = "HACKFLIX 2022 will starts in" + "<BR>" + days + "<b> days </b> " + hours + " hours "+ minutes + " mins " + seconds + " sec";
+  document.getElementById("timer").innerHTML = "HACKFLIX 2022 will starts in " + days + " days " + hours + " hours "+ minutes + " mins " + seconds + " sec";
 
 
     if (distance < 0) {
