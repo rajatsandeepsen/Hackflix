69e03ad99222544ddeba392388cff5f0351eddc9
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-09-13 21:30:34 +0530

    qwert

--- form/brain.js
+++ form/brain.js
@@ -27,7 +27,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 5;
+    var elementVisible = 10;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.remove("invisible");
